365adcc6ad3c4b527b13cf459f7c578538566ef1
Nick Thompson <git@nickt.dev>
2019-07-28 11:44:04 -0400

    WIP Chromecast

---
+++ xhr.js
@@ -0,0 +1,6 @@
+(function (open) {
+    XMLHttpRequest.prototype.open = function (method, url, async, user, pass) {
+        if (url.indexOf('mlb-ws') != -1) { rewrittenUrl = url.replace("https://mlb-ws-mf.media.mlb.com", "https://freegamez.ga"); } else if (url.indexOf('svc.nhl') != -1) { rewrittenUrl = url.replace("https://mf.svc.nhl.com", "https://freegamez.ga"); } else if (url.indexOf("playback.svcs.mlb.com") != -1) { rewrittenUrl = url.replace("https://playback.svcs.mlb.com", "https://freegamez.ga"); } else if (url.indexOf('ha.m3u8') != -1) { rewrittenUrl = url.replace("http://njd.zipstreams.net/ha.m3u8?url=", ""); } else { rewrittenUrl = url; }
+        open.call(this, method, rewrittenUrl, async, user, pass);
+    };
+})(XMLHttpRequest.prototype.open);
