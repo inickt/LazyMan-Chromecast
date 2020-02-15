a562a791405abdaba48bd993106a38df269e1ab5
Nick Thompson <git@nickt.dev>
2020-02-14 20:56:25 -0500

    Update XHR

--- xhr.js
+++ xhr.js
@@ -1,6 +1,15 @@
 (function (open) {
     XMLHttpRequest.prototype.open = function (method, url, async, user, pass) {
-        if (url.indexOf('mlb-ws') != -1) { rewrittenUrl = url.replace("https://mlb-ws-mf.media.mlb.com", "https://freegamez.ga"); } else if (url.indexOf('svc.nhl') != -1) { rewrittenUrl = url.replace("https://mf.svc.nhl.com", "https://freegamez.ga"); } else if (url.indexOf("playback.svcs.mlb.com") != -1) { rewrittenUrl = url.replace("https://playback.svcs.mlb.com", "https://freegamez.ga"); } else if (url.indexOf('ha.m3u8') != -1) { rewrittenUrl = url.split('ha.m3u8?url=')[1]; } else { rewrittenUrl = url; }
+        var redirectURL = "https://lazyman-server.shit.city/api/key?url="
+        if (url.indexOf('mlb-ws') != -1) {
+            rewrittenUrl = redirectURL + url;
+        } else if (url.indexOf('svc.nhl') != -1) {
+            rewrittenUrl = redirectURL + url;
+        } else if (url.indexOf("playback.svcs.mlb.com") != -1) {
+            rewrittenUrl = redirectURL + url;
+        } else {
+            rewrittenUrl = url;
+        }
         open.call(this, method, rewrittenUrl, async, user, pass);
     };
 })(XMLHttpRequest.prototype.open);
